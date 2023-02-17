3f2c24416b0e7c31f525f3794b19790e278b13a5
aneozenganani <wilsonanani@hotmail.com>
2023-02-16 20:10:17 -0800

    compile

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
-#include <stdio.h> 
-
+#include <stdio.h>
 /**
- * main - Entry point
- *
- *
- * Return: Always 0 (success) 
+ * main - Entry
+ * Return: Always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
